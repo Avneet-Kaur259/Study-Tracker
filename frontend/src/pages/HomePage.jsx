b0491131ea6cfdfcfed866ef5988e8c1adb5d3ac
Avneet Kaur <avneet2528@gmail.com>
2025-10-30 21:05:01 +0530

    Home page updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,37 +1,17 @@
 import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import RateLimitedUI from "../components/RateLimitedUI"
-import toast from "react-hot-toast"
 import NoteCard from "../components/NoteCard"
-import api from "../lib/axios"
 import NotesNotFound from "../components/NotesNotFound"
+import { useNoteStore } from "../store/useNoteStore"
 
 const HomePage = () => {
-    const [isRateLimited, setIsRateLimited] = useState(false)
-    const [notes, setNotes] = useState([])
-    const [loading, setLoading] = useState(true)
+    const { fetchNotes, notes, loading, isRateLimited } = useNoteStore()
 
     useEffect(() => {
-        const fetchNotes = async () => {
-            try {
-                const res = await api.get("/notes")
-                console.log(res.data)
-                setNotes(res.data)
-                setIsRateLimited(false)
-            } catch (error) {
-                console.log("Error fetching notes")
-                console.log(error.response)
-                if (error.response?.status === 429) {
-                    setIsRateLimited(true)
-                } else {
-                    toast.error("Failed to load notes")
-                }
-            } finally {
-                setLoading(false)
-            }
-        }
+        console.log("notes:", notes)
         fetchNotes()
-    }, [])
+    }, [fetchNotes])
 
     return (
         <div className="min-h-screen">
@@ -47,7 +27,7 @@ const HomePage = () => {
                 {notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {notes.map((note) => (
-                            <NoteCard key={note._id} note={note} setNotes={setNotes} />
+                            <NoteCard key={note._id} note={note} />
                         ))}
                     </div>
                 )}
