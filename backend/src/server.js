57ff6854b96ce649552b851155f639c9288cbd1c
Avneet Kaur <avneet2528@gmail.com>
2025-11-01 22:15:59 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path, { join } from "path";
 
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -13,11 +14,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }));
+}
 app.use(express.json());
 app.use(cookieParser());
 app.use(rateLimiter);
@@ -25,6 +29,14 @@ app.use(rateLimiter);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
